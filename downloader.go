898fa7b979bbad373aac55743f6d73666ffbacfb
Restart <45609733+RestartFU@users.noreply.github.com>
2024-06-23 17:18:17 -0400

    initial commit

---
+++ downloader.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"archive/zip"
+	"fmt"
+	"github.com/bedrockmyshit/backend/app/minecraft"
+	"github.com/joho/godotenv"
+	"github.com/povsister/scp"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func init() {
+	_ = godotenv.Load(".env")
+}
+
+func main() {
+	args := os.Args
+	dir := filepath.Dir(args[0])
+	if len(args) < 3 {
+		after, _ := strings.CutPrefix(args[0], dir+"\\")
+		fmt.Printf("Usage: %s <ip:port> <ssh_addr>\n", after)
+		return
+	}
+	serverAddr := args[1]
+
+	split := strings.Split(serverAddr, ":")
+	if len(split) != 2 {
+		fmt.Println("Invalid ip:port")
+		return
+	}
+	addr, port := split[0], split[1]
+
+	username := os.Getenv("SSH_USERNAME")
+	if username == "" {
+		fmt.Println("SSH_USERNAME not set")
+		return
+	}
+	password := os.Getenv("SSH_PASSWORD")
+	if password == "" {
+		fmt.Println("SSH_PASSWORD not set")
+		return
+	}
+
+	conn, err := minecraft.Connect(serverAddr)
+	if err != nil {
+		fmt.Printf("Failed to connect to %s: %v\n", serverAddr, err)
+		return
+	}
+	defer conn.Close()
+
+	packs := conn.ResourcePacks()
+	if len(packs) == 0 {
+		fmt.Printf("No packs found on %s\n", serverAddr)
+		return
+	}
+	tempPath := os.Getenv("TEMP")
+
+	f, err := os.Create(fmt.Sprintf("%s/%s.zip", tempPath, addr))
+	if err != nil {
+		_ = f.Close()
+		fmt.Printf("Failed to create file: %v\n", err)
+		return
+	}
+	zipFile := zip.NewWriter(f)
+
+	for _, pack := range packs {
+		buf, err := minecraft.EncodePack(pack)
+		if err != nil {
+			fmt.Printf("Failed to encode pack: %v\n", err)
+			continue
+		}
+		if pack.Encrypted() {
+			buf, err = minecraft.DecryptPack(buf, pack.ContentKey())
+			if err != nil {
+				fmt.Printf("Failed to decrypt pack: %v\n", err)
+				continue
+			}
+		}
+
+		p, err := zipFile.Create(pack.Name() + ".zip")
+		if err != nil {
+			fmt.Printf("Failed to create zip file: %v\n", err)
+			continue
+		}
+		if _, err = p.Write(buf); err != nil {
+			fmt.Printf("Failed to write to zip file: %v\n", err)
+			continue
+		}
+	}
+	f.Close()
+	zipFile.Close()
+	fmt.Printf("Packs saved to %s\n", f.Name())
+
+	sshAddr := args[2]
+	sshConf := scp.NewSSHConfigFromPassword(username, password)
+	scpClient, err := scp.NewClient(sshAddr, sshConf, &scp.ClientOption{})
+	defer scpClient.Close()
+	if err != nil {
+		fmt.Printf("Failed to connect to %s: %v\n", args[2], err)
+		return
+	}
+	fmt.Printf("Copying file %s to remote: %s\n", f.Name(), sshAddr)
+
+	err = scpClient.CopyFileToRemote(f.Name(), fmt.Sprintf("/root/decryptmypack.com/packs/%s/%s/%s.zip", addr, port, addr), &scp.FileTransferOption{})
+	if err != nil {
+		fmt.Printf("Failed to copy file to remote: %v\n", err)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,34 @@
+module github.com/bedrockmyshit/auto-download-upload
+
+go 1.22.2
+
+require (
+	github.com/bedrockmyshit/backend v0.0.0-20240623201440-28f9ce51496e
+	github.com/joho/godotenv v1.5.1
+	github.com/povsister/scp v0.0.0-20210427074412-33febfd9f13e
+)
+
+require (
+	github.com/go-gl/mathgl v1.1.0 // indirect
+	github.com/go-jose/go-jose/v3 v3.0.3 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.9 // indirect
+	github.com/kr/pretty v0.3.1 // indirect
+	github.com/muhammadmuzzammil1998/jsonc v1.0.0 // indirect
+	github.com/patrickmn/go-cache v2.1.0+incompatible // indirect
+	github.com/pelletier/go-toml/v2 v2.0.7 // indirect
+	github.com/restartfu/gophig v0.0.0-20230501130741-bd994da592f7 // indirect
+	github.com/sandertv/go-raknet v1.14.1 // indirect
+	github.com/sandertv/gophertunnel v1.38.1-0.20240616191646-0124e15a718b // indirect
+	github.com/txthinking/runnergroup v0.0.0-20210608031112-152c7c4432bf // indirect
+	github.com/txthinking/socks5 v0.0.0-20230325130024-4230056ae301 // indirect
+	golang.org/x/crypto v0.24.0 // indirect
+	golang.org/x/image v0.17.0 // indirect
+	golang.org/x/net v0.26.0 // indirect
+	golang.org/x/oauth2 v0.21.0 // indirect
+	golang.org/x/sys v0.21.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
