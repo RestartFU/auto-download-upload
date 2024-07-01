4a1c912d3fecb259f42a807c25ac2f1463e89ab2
Restart <45609733+RestartFU@users.noreply.github.com>
2024-07-01 14:30:32 -0400

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,15 @@
 @echo off
-set servers=geo.hivebedrock.network:19132 play.rustmc.online:19132
+set servers=play.craftia.me:19132 realmc.xyz:19132 tetramc.org:19132 soulmine.ru:19132 skill-mine.ru:19133 play.rustmc.online:19132 geo.hivebedrock.network:19132
+set len=0
 
 (for %%s in (%servers%) do (
-   start /wait downloader.exe %%s decryptmypack.com
+    set /a len+=1
 ))
+
+ :loop
+    (for %%s in (%servers%) do (
+       start /B /wait downloader.exe %%s decryptmypack.com
+        timeout /t 600 /nobreak >nul
+        echo[
+    ))
+ goto loop
